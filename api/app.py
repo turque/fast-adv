3da671d0c06928825c9f8a1d1b9cbe306a5d1bdd
Arnaldo Turque <arnaldo@turque.com.br>
2023-10-04 22:54:35 -0300

    configuração do projeto para utilizar o poetry

---
+++ api/app.py
@@ -0,0 +1,43 @@
+import os
+
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+path = os.getcwd()
+
+
+@app.get('/api/python')
+def hello_world():
+    return {'message': 'Hello World'}
+
+
+@app.get('/api/img/{filename}')
+def get_img(filename: str):
+    filepath = os.path.join(f'{path}/public/img/', os.path.basename(filename))
+    return FileResponse(filepath)
+
+
+@app.post('/api/login')
+async def login(request: Request):
+    print(request)
+    return {'message': 'Logado!'}
+
+
+@app.get('/api/auth/login')
+async def read_item(request: Request):
+    print(dict(request))
+    return {
+        'user': {
+            'id': 1,
+            'username': 'arnaldo@turque.com.br',
+            'email': 'arnaldo@turque.com.br',
+            'fullname': 'Arnaldo Turque',
+            'role': 'SUPER',
+            'createdAt': '2021-05-30T06:45:19.000Z',
+            'name': 'Arnaldo Turque',
+            'avatar': '/img/perfil-roxo.png',
+        },
+        'token': 'ey...',
+    }
--- api/security.py
+++ api/security.py
@@ -3,16 +3,16 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
+SECRET_KEY = 'your-secret-key'
+ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({'exp': expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
