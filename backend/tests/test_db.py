dbb2e4dc216cb48a198d4c8331857bfa4c381c2b
Arnaldo Turque <arnaldo@turque.com.br>
2023-11-07 23:50:23 -0300

    Tests and rename invite

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from sqlalchemy import select
 
-from api.models import Team, User
+from api.models import Invite, Race, Team, User
 
 
 def test_create_user(session):
@@ -27,3 +29,42 @@ def test_create_team(session, user: User):
     user = session.scalar(select(User).where(User.id == user.id))
 
     assert team in user.teams
+
+
+def test_create_invite(session, user: User, team: Team):
+    invite = Invite(
+        token='123456789abcdef',
+        name='athlete1',
+        email='athlete1@email.com',
+        team=team.id,
+        user_id=user.id,
+    )
+
+    session.add(invite)
+    session.commit()
+    session.refresh(invite)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert invite in user.invites
+
+
+def test_create_race(session, user: User):
+    race = Race(
+        name='Race 1',
+        place='anywhere',
+        race_date=datetime.now(),
+        distance=100,
+        url_race='race1.com',
+        race_description='description',
+        place_description='description',
+        observations='description',
+        user_id=user.id,
+    )
+    session.add(race)
+    session.commit()
+    session.refresh(race)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert race in user.races
