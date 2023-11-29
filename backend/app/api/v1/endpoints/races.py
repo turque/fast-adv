068c0a3ed5b19a7de773aca7318eda81ff5533db
Arnaldo Turque <arnaldo@turque.com.br>
2023-11-29 13:30:22 -0300

    devel

--- backend/app/api/v1/endpoints/races.py
+++ backend/app/api/v1/endpoints/races.py
@@ -2,7 +2,6 @@ from typing import Annotated, Any, List
 
 from fastapi import APIRouter, Depends, HTTPException
 
-# from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
