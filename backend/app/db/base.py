75823a74caced2cf934795d3f6d3435d5b4719b0
Arnaldo Turque <arnaldo@turque.com.br>
2024-03-18 14:59:12 -0300

    base model update

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,10 +1,2 @@
 from app import models  # noqa: F401
 from app.db.base_class import Base  # noqa: F401
-
-# from app.models.invite import Invite
-# from app.models.race import Race
-# from app.models.strategic_planning import StrategicPlanning
-# from app.models.team import Team
-# from app.models.user import User
-
-# __all__ = ['Base', 'Invite', 'Race', 'Team', 'User', 'StrategicPlanning']
