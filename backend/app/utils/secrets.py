bdc701851abe5fb5e02623aa97a592c6c6a031d4
F1299859 Arnaldo Davino Godinho Turque <arnaldoturque@bb.com.br>
2024-07-10 14:29:51 -0300

    geração dsenha e token seguros

---
+++ backend/app/utils/secrets.py
@@ -0,0 +1,13 @@
+import secrets
+import string
+
+def generate_password(length: int = 20) -> str:
+
+    if not isinstance(length, int) or length < 8:
+        raise ValueError("The password must be at least 8 characters long")
+    
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(alphabet) for i in range(length))
+
+def generate_urlsafe() -> str:
+    return secrets.token_urlsafe()
