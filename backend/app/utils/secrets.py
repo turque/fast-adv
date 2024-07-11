3bca98a330f6329ab19387eb5918db8bebc0d077
Arnaldo Turque <arnaldo@turque.com.br>
2024-07-11 00:22:24 -0300

    remoção de pontuação do gerador de senha

--- backend/app/utils/secrets.py
+++ backend/app/utils/secrets.py
@@ -7,7 +7,7 @@ def generate_password(length: int = 20) -> str:
     if not isinstance(length, int) or length < 8:
         raise ValueError('The password must be at least 8 characters long')
 
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+    alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for i in range(length))
 
 
